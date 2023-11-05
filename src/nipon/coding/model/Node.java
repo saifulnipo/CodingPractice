ced5ce722b2d78afbd514a632c8219d742fa6ec4
AQM Saiful Islam <saiful.nipo@gmail.com>
2023-11-05 23:36:38 +0100

    Add more constract option for node class

--- src/nipon/coding/model/Node.java
+++ src/nipon/coding/model/Node.java
@@ -7,7 +7,11 @@ public class Node {
 
 	public Node(int value) {
 		this.value = value;
-		this.left = null;
-		this.right = null;
+	}
+
+	public Node(int value, Node left, Node right) {
+		this.value = value;
+		this.left = left;
+		this.right = right;
 	}
 }
